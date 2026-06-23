eb9a39e7f774549848141d020e88d4c9c96162fb
vamshi814 <vamshi814.s@gmail.com>
2026-06-23 11:19:00 +0530

    1-Run () , app.Run

--- Middleware/Program.cs
+++ Middleware/Program.cs
@@ -1,6 +1,28 @@
+//creating an instance of the web application builder
 var builder = WebApplication.CreateBuilder(args);
+
+//create an instance of the web application(ASP.NET Core application)
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//server knows how to handle incoming requests and send responses back to the client
+//app.MapGet("/", () => "Hello World!");
 
-app.Run();
+
+//run the application and start listening for incoming requests
+
+//our ASP.NET Core application will be hosted on a web server
+//and will be able to handle incoming HTTP requests and send responses back to the client
+
+//httpcontext is an object that represents the current HTTP request and response
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("Hosted on Server \n" +
+        "httpcontext is an object that represents the current HTTP request and response");
+});
+
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("Next Middleware");
+});
+
+app.Run();//start the application and listen for incoming requests
